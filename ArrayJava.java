3c996c5b14e897a303f37c8fa4e7387a940e7e8d
Sushilkumar <sushilkumartondikatti02@gmail.com>
2026-03-02 15:21:01 +0530

    Add ArrayJava class to create and display an array

---
+++ ArrayJava.java
@@ -0,0 +1,18 @@
+import java.util.Scanner
+public class ArrayJava{
+  public static void main(String args[])
+  {
+    System.out.println("Enter the size of array");
+    Scanner sc=new Scanner(System.In);
+    int n=sc.nextInt();
+    int a[]=new int[n];
+    for(int i=0;i<n;i++)
+      {
+        a[i]=i*5;
+      }
+    for(int j=0;j<n;j++)
+      {
+        System.out.println("array element "+j+"is"+a[j]);
+      }
+  }
+}
